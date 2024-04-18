5bc0f99dd7cd7c25c0e951a85818d3327595ef03
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-04-18 15:37:49 +0700

    Update Profile.tsx

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -7,6 +7,7 @@ import { useQuery } from 'react-query'
 import profileApi from 'src/apis/profile.api'
 import { useMemo } from 'react'
 import { User } from 'src/types/users.type'
+import GameHistory from './components/GameHistory'
 interface ProfileProps {}
 
 const Profile: React.FC<ProfileProps> = () => {
@@ -46,46 +47,9 @@ const Profile: React.FC<ProfileProps> = () => {
         <h2 className='text-lg font-bold flex flex-col items-center'>Các ván đấu</h2>
         <p className='text-base-content p-5'>Không có ván đấu nào đang diễn ra</p>
       </div>
-      <div className='mt-5 bg-base-200'>
-        <h2 className='text-lg font-bold pl-5'>Các ván cờ đã kết thúc</h2>
-        <table className='w-full'>
-          <thead>
-            <tr>
-              <th className='p-3 w-1/5'></th>
-              <th className='p-3 w-2/5'>Các kỳ thủ</th>
-              <th className='p-3 w-1/5'>Kết quả</th>
-              <th className='p-3 w-1/5'>Ngày</th>
-            </tr>
-          </thead>
-          <tbody className='w-full'>
-            <tr>
-              <td className='p-3 w-1/5'>
-                <FaGamepad className='text-blue-500' size={28} />
-              </td>
-              <td className='p-3 w-2/5'>
-                <p className='flex items-center'>
-                  <FaSquare className='text-green-500 inline-block align-middle mr-1' />
-                  <span>Martinvelo</span> <span className='text-xs'>(432)</span>
-                  <Link to={`/chat/:id`}>
-                    <BiMessage className='ml-2' />
-                  </Link>
-                </p>
-                <p className='flex items-center'>
-                  <FaSquare className='text-green-500 inline-block align-middle mr-1' />
-                  <span>thuuhee</span> <span className='text-xs'>(232)</span>
-                </p>
-              </td>
-              <td className='p-3 flex items-center justify-center w-1/5'>
-                <span className='flex items-center'>
-                  <p>1</p>
-                  <FaMinusSquare className='text-red-500 m-2' />
-                  <p>0</p>
-                </span>
-              </td>
-              <td className='p-3 flex items-center justify-center w-1/5'></td>
-            </tr>
-          </tbody>
-        </table>
+      <div className='mt-5'>
+        <h2 className='text-lg font-bold pl-5'>Previous games</h2>
+        <GameHistory />
       </div>
     </div>
   )
