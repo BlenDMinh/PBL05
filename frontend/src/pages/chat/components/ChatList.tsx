1c86d83c249caa51464ba63f42817e12935ce6c2
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-04-19 09:56:48 +0700

    chat layout

--- frontend/src/pages/chat/components/ChatList.tsx
+++ frontend/src/pages/chat/components/ChatList.tsx
@@ -1,24 +1,28 @@
-import { useChat } from "src/contexts/chat.context";
-import Conversation from "./Conversation";
-import { useEffect } from "react";
-import { getSessionIdFromLS } from "src/utils/auth";
-import { ReadyState } from "react-use-websocket";
-import { useParams } from "react-router-dom";
+import { useChat } from 'src/contexts/chat.context'
+import Conversation from './Conversation'
+import { useEffect } from 'react'
+import { getSessionIdFromLS } from 'src/utils/auth'
+import { ReadyState } from 'react-use-websocket'
+import { useParams } from 'react-router-dom'
 
 export default function ChatList() {
-    const { id } = useParams()
-    const chat = useChat()
-    useEffect(() => {
-        chat.startChat(getSessionIdFromLS())
-    }, [])
-    if(chat.state != ReadyState.OPEN) {
-        return (<div className="bg-base-300 h-full w-full flex items-center justify-center">
-            <span className="loading loading-lg loading-ring"></span>
-        </div>)
-    }
-    return (<div className="bg-base-300 h-full w-full overflow-y-auto p-1 gap-1">
-        {
-            chat.conversations.map(c => <Conversation key={c.id} conversation={c} active={id == c.id.toString()}/>)
-        }
-    </div>)
+  const { id } = useParams()
+  const chat = useChat()
+  useEffect(() => {
+    chat.startChat(getSessionIdFromLS())
+  }, [])
+  if (chat.state != ReadyState.OPEN) {
+    return (
+      <div className='bg-base h-full w-full flex items-center justify-center'>
+        <span className='loading loading-lg loading-ring'></span>
+      </div>
+    )
+  }
+  return (
+    <div className='bg-base h-full w-full overflow-y-auto p-1 gap-1'>
+      {chat.conversations.map((c) => (
+        <Conversation key={c.id} conversation={c} active={id == c.id.toString()} />
+      ))}
+    </div>
+  )
 }
