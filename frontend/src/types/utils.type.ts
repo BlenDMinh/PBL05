72646ded22300e56956f6d296c5d6d307d67f373
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-04-21 11:56:40 +0700

    fix

--- frontend/src/types/utils.type.ts
+++ frontend/src/types/utils.type.ts
@@ -2,3 +2,26 @@ export interface PaginitionResponse {
   totalElements: number,
   totalPages: number
 }
+
+export interface SuccessResponse<Data> {
+  message: string
+  data: Data
+}
+export interface ErrorResponse<Data> {
+  message: string
+  data?: Data
+}
+
+export type NoUndefinedField<T> = {
+  [P in keyof T]-?: NoUndefinedField<NonNullable<T[P]>>
+}
+
+export type Nameable = {
+  name?: string
+  full_name?: string
+}
+
+export interface SelectOption {
+  label: string
+  value: string | number
+}
