da3f97068f4e5e09c69b83fdf06fc38cd2eeac51
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-04-21 12:00:56 +0700

    upgrade-insecure-requests

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,6 +16,7 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AppContextProvider>
