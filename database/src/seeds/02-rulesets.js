2f4db1954f76f1817aa5d9e435637e168d736b89
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2024-05-09 19:32:11 +0700

    feat: add more game rule seeds

--- database/src/seeds/02-rulesets.js
+++ database/src/seeds/02-rulesets.js
@@ -8,14 +8,34 @@ exports.seed = async function (knex) {
   await knex(tableName).del();
   await knex(tableName).insert([
     {
-      id: 1,
-      name: "Chơi tự do",
+      name: "Chơi 10 phút",
       detail: {
-        minute_per_turn: 0,
-        total_minute_per_player: 0,
-        turn_around_steps: 0,
-        turn_around_time_plus: 0,
+        minute_per_turn: 3,
+        total_minute_per_player: 10,
+        turn_around_steps: -1,
+        turn_around_time_plus: -1,
       },
+      published: true,
+    },
+    {
+      name: "Chơi 20 phút",
+      detail: {
+        minute_per_turn: 3,
+        total_minute_per_player: 20,
+        turn_around_steps: -1,
+        turn_around_time_plus: -1,
+      },
+      published: true,
+    },
+    {
+      name: "Chơi siêu tốc",
+      detail: {
+        minute_per_turn: 10,
+        total_minute_per_player: 10,
+        turn_around_steps: 2,
+        turn_around_time_plus: 5,
+      },
+      published: true,
     },
   ]);
 };
